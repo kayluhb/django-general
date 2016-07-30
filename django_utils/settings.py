a148b413ff41b3fc78f74b5f0cc623a8bd15404a
Caleb Brown <killerdillercaleb@gmail.com>
2016-07-30 11:34:12 -0500

    Settings update

--- django_utils/settings.py
+++ django_utils/settings.py
@@ -4,8 +4,17 @@ from django.conf import settings
 
 
 DJANGO_UTILS_PER_PAGE = getattr(
-    settings, 'DJANGO_UTILS_PER_PAGE', 25)
+    settings,
+    'DJANGO_UTILS_PER_PAGE',
+    25
+)
 DJANGO_UTILS_HTML5_DATE_FORMAT = getattr(
-    settings, 'DJANGO_UTILS_HTML5_DATE_FORMAT', '%Y-%m-%d')
+    settings,
+    'DJANGO_UTILS_HTML5_DATE_FORMAT',
+    '%Y-%m-%d'
+)
 DJANGO_UTILS_HTML5_DATETIME_FORMAT = getattr(
-    settings, 'DJANGO_UTILS_HTML5_DATETIME_FORMAT', '%Y-%m-%d %H:%M:%S')
+    settings,
+    'DJANGO_UTILS_HTML5_DATETIME_FORMAT',
+    '%Y-%m-%d %H:%M:%S'
+)
