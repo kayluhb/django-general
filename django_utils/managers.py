ce7d63180d8af0f3c45ee28e64893a603209c33a
Caleb Brown <killerdillercaleb@gmail.com>
2016-08-01 16:18:46 -0500

    Rework the status manager so there isn't recursive importing

--- django_utils/managers.py
+++ django_utils/managers.py
@@ -4,16 +4,27 @@ import datetime
 
 from django.db import models
 
-import .models as django_utils_models
+# from django_utils.models import (PUBLISHED, DRAFT, HIDDEN)
 
 
 class StatusManager(models.Manager):
-    """ This adds a query method to pull all published records. """
+    """ This adds a query methods to pull records based on status.
+    """
+
+    def draft(self):
+        """ This adds a query method to pull all draft records.
+        """
+        return super(StatusManager, self).filter(
+            status=django_utils.models.DRAFT)
+
     def published(self):
+        """ This adds a query method to pull all published records that are
+        not expired.
+        """
         today = datetime.date.today()
 
         return super(StatusManager, self).filter(
-            status=django_utils_models.PUBLISHED,
+            status=django_utils.models.PUBLISHED,
             pub_date__lte=today,
         ).filter(
             models.Q(expire_date__gt=today) |
@@ -21,12 +32,15 @@ class StatusManager(models.Manager):
         )
 
     def published_or_hidden(self):
+        """ This adds a query method to pull all published or hidden records
+        that are not expired
+        """
         today = datetime.date.today()
 
         return super(StatusManager, self).filter(
             status__in=[
-                django_utils_models.PUBLISHED,
-                django_utils_models.HIDDEN
+                django_utils.models.PUBLISHED,
+                django_utils.models.HIDDEN
             ],
             pub_date__lte=today,
         ).filter(
