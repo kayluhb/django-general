f8351ccf760b783cd77217ce8ac203491602985f
Caleb Brown <killerdillercaleb@gmail.com>
2016-08-02 12:05:28 -0500

    Add settings to get autodocs running

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,13 @@ cwd = os.getcwd()
 parent = os.path.dirname(cwd)
 sys.path.append(parent)
 
+import django
 import django_utils
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings_docs'
+
+django.setup()
+
 
 # -- General configuration -----------------------------------------------------
 
@@ -100,7 +106,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
